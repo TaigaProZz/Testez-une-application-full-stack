23258cac54084d1c108347527fd8660aaeeb7713
Florian <74510014+TaigaProZz@users.noreply.github.com>
2025-04-01 13:52:41 +0200

    (feat) e2e login component

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -24,4 +24,46 @@ describe('Login spec', () => {
 
     cy.url().should('include', '/sessions')
   })
+
+  it('Login email is empty', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email]').focus()
+
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.get('button[type=submit]').should('be.disabled')
+
+    cy.get('button[type=submit]').should('be.disabled')
+    cy.get('p').should('have.class', 'error')
+  })
+
+  it('Login password is empty', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email]').type("a@g.com")
+    cy.get('input[formControlName=password]')
+      .focus()
+      .type(`{enter}{enter}`)
+
+    cy.get('button[type=submit]').should('be.disabled')
+    cy.get('p').should('have.class', 'error')
+  })
+
+  it('Login error with invalid credentials', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {
+        message: 'Invalid credentials',
+      },
+    }).as('loginRequest')
+
+    cy.get('input[formControlName=email]').type("a@a.a")
+    cy.get('input[formControlName=password]').type("a{enter}{enter}")
+
+    cy.wait('@loginRequest')
+
+    cy.get('p').should('have.class', 'error')
+  })
 });
